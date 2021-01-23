b75f5730960e1102bb0d0add292af92a5d1df5af
jmylinner <jmylinner@163.com>
2021-01-23 21:03:25 +0800

    配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 module.exports = {
     assetsDir: 'assets', //静态资源目录（js, css, img）
     lintOnSave: false, //是否开启eslint
+    devServer: {
+        host: 'localhost',        //设置本地服务器   选填
+        port: 9528,				//设置本地默认端口  选填
+        proxy: {                 //设置代理，必须填
+            '/api': {              //设置拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
+                target: 'http://127.0.0.1:9969',     //代理的目标地址
+                changeOrigin: true,              //是否设置同源，输入是的
+                pathRewrite: {                   //路径重写
+                    '^/api': ''                     //选择忽略拦截器里面的单词
+                }
+            }
+        }
+    }
 }
